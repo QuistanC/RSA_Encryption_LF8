05ed041a1eb3110bbb8cd414d36f941eb6f81ba8
LisaK <lisa.kortkamp@viscom.de>
2025-06-27 08:36:08 +0200

    WIP

---
+++ Utilities/Operations.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RSA_Encryption.Utilities;
+
+public class Operations
+{
+    public static long generateModulus(int primeOne, int primeTwo)
+    {
+        long modulus = primeTwo * primeOne;
+        return modulus;
+    }
+
+    public static int generateEulers(int primeOne, int primeTwo)
+    {
+        return (primeOne-1)*(primeTwo-1);
+    }
+}
