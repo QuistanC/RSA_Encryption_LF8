4c459b20c26cb4adc2679c30f010bad5ad2d8789
LisaK <lisa.kortkamp@viscom.de>
2025-06-27 08:58:42 +0200

    added operations

--- Utilities/Operations.cs
+++ Utilities/Operations.cs
@@ -10,14 +10,42 @@ namespace RSA_Encryption.Utilities;
 
 public class Operations
 {
-    public static long generateModulus(int primeOne, int primeTwo)
+    public static long generateModulus(long primeOne, long primeTwo)
     {
-        long modulus = primeTwo * primeOne;
-        return modulus;
+        return primeTwo * primeOne;
     }
 
-    public static int generateEulers(int primeOne, int primeTwo)
+    public static long generateEulers(long primeOne, long primeTwo)
     {
         return (primeOne-1)*(primeTwo-1);
     }
+
+    public static long generatePrivateKey(long publicKey, long eulers)
+    {
+        return publicKey * eulers; //TEMP
+    }
+
+    private static long findGreatestCommonDividor(long primeOne, long primeTwo)
+    {
+        if (primeOne == 0 || primeTwo == 0)
+            return 0;
+
+        if(primeOne == primeTwo)
+        {
+            return primeOne;
+        }
+
+        if (primeOne > primeTwo)
+        {
+            return findGreatestCommonDividor(primeOne-primeTwo, primeTwo);
+        }
+
+        return findGreatestCommonDividor(primeOne, primeTwo-primeOne);
+    }
+
+    private static bool checkIfPrime(long primeOne, long primeTwo)
+    {
+        return findGreatestCommonDividor(primeOne, primeTwo) == 1;
+    }
+
 }
