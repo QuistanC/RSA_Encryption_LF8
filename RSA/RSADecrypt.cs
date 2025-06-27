31248e62c651329e71db99434b1b619c8de15294
Max Nolting <max.nolting@viscom.de>
2025-06-27 09:31:24 +0200

    basic RSA En-/Decrypt classes

---
+++ RSA/RSADecrypt.cs
@@ -0,0 +1,13 @@
+﻿
+using System.Numerics;
+
+namespace RSA_Encryption.RSA;
+
+internal class RSADecrypt(BigInteger d)
+{
+    private BigInteger D = d;
+    internal Byte[] Decrypt(Byte[] input)
+    {
+        throw new NotImplementedException();
+    }
+}
