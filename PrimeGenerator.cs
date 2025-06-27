43a247c329538572271b9829e9c03b0c97d9f0e2
Max Nolting <max.nolting@viscom.de>
2025-06-27 08:35:54 +0200

    changed return type

--- PrimeGenerator.cs
+++ PrimeGenerator.cs
@@ -3,8 +3,17 @@ namespace RSA_Encryption;
 
 internal class PrimeGenerator
 {
-    internal int[] GeneratePrimePair()
+    internal long[] GeneratePrimePair()
     {
+        var PrimeList = new List<long>();
+        var random = new Random();
+
+        while (PrimeList.Count < 2)
+        {
+            var candidate = (long)random.NextDouble() * Math.Pow(10,10308);
+            PrimeList.Add(0);
+        }
+
         return [2, 3];
     }
 }
